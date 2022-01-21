7e38cddd59f600413359812c2bc41635dc3b5f97
Ebenezer Ashong <ebenezer.ashong@amalitech.org>
2022-01-21 15:55:08 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
+import AppContext from './AppContext';
+import { useState, useEffect } from 'react';
+import questionsArray from './constants/questionsArray';
+import Questions from './components/Questions';
 
 function App() {
+
+  let [questions, setQuestions] = useState([]);
+  let [answers, setAnswers] = useState([]);
+  let [questionAnswer, setQuestionAnswer] = useState({});
+
+  //call setQuestions and setQuestionAnswer once per render
+  useEffect(() => {
+    setQuestions(questionsArray);
+    setQuestionAnswer(questionsArray[0]);
+  }, []);
+
+  //handle text changes
+  let handleChangeInput = (e) => {
+    setQuestionAnswer({
+      ...questionAnswer,
+      answer: e.target.value,
+    });
+  };
+
+//next question button click, input is set to setAnswers
+  let nextQuestion = (e) => {
+    e.preventDefault();
+    questions.map((question) => {
+      if (question.resumeFieldId == questionAnswer.resumeFieldId) {
+        setAnswers([
+          ...answers,
+          { ...question, answer: questionAnswer.answer },
+        ]);
+      }
+    });
+
+    //moves to next question index
+    questions.map((qa, index) => {
+      if (index <= questions.length) {
+        if (qa.resumeFieldId === questionAnswer.resumeFieldId) {
+          setQuestionAnswer(questions[index + 1]);
+        }
+      }
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContext.Provider value={{
+      state: {
+        questionAnswer,
+        questions,
+        answers,
+      },
+      function: {
+        handleChangeInput: handleChangeInput,
+        nextQuestion: nextQuestion,
+      },
+    }}>
+      <Questions/>
+    </AppContext.Provider>
   );
 }
 
