769ac5fdf7a31183784dd1fef9de2a8d14236a1a
Ebenezer Ashong <ebenezer.ashong@amalitech.org>
2022-01-22 15:59:57 +0000

    update show cv on different page

---
+++ src/components/Cv/Cv.js
@@ -0,0 +1,32 @@
+
+
+const Cv = ({personDetails}) =>{
+    let selector = 0;
+    if(personDetails.length > 1){
+        selector = personDetails.length - 1;
+    }
+
+    return(  
+        <>
+        <section>
+            <h3>{personDetails[selector].name}</h3>
+            <h4>{personDetails[selector].address}</h4>
+            <h4>{personDetails[selector].phoneNumber}</h4>
+            <h4>{personDetails[selector].email}</h4>
+            <h4>{personDetails[selector].linkedInLink}</h4>
+        </section>
+        <section>
+            <h2>Education</h2>
+            <p>{personDetails[selector].education}</p>
+            <p>{personDetails[selector].education1}</p>
+        </section>
+        <section>
+            <h2>Experience</h2>
+            <p>{personDetails[selector].experience}</p>
+            <p>{personDetails[selector].experience1}</p>
+        </section>
+        </>
+    )
+}
+
+export default Cv;
